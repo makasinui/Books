dba1cc6556a96f3677213ca05a3c5405daea98b6
Makasinui <mks.shi@ya.ru>
2022-10-27 18:28:04 +0300

    fixes for pagination and search

--- resources/js/components/Pagination.vue
+++ resources/js/components/Pagination.vue
@@ -1,57 +1,42 @@
 <template>
-        <nav class="justify-center flex">
-            <ul class="pagination">
-                
-                    <li class="page-item"
-                    
-                    v-for="(item,i) in Math.ceil(pages/5)"
-                    @click="active(item)"
-                    :class="{'active':arr[item]}"
-                    >
-                        <a class="page-link" @click="$emit('selectPage',i+1)" href="#">{{+item}}</a>
-                    </li>
-
-
-
-               
-            </ul>
-        </nav>
+    <nav class="justify-center flex">
+        <ul class="pagination">
+            <li
+                class="page-item"
+                v-for="(item, i) in Math.ceil(pages / 5)"
+                @click="active(item)"
+                :class="{ active: item === currentPage }"
+            >
+                <a
+                    class="page-link"
+                    @click="$emit('selectPage', i + 1)"
+                    >{{ item }}</a
+                >
+            </li>
+        </ul>
+    </nav>
 </template>
 <script>
 export default {
-    props:{
-        pages:''
+    props: {
+        pages: {
+            required: true
+        },
     },
-    data(){
-        return{
-            arr:[],
-            isClick:false
-        }
-        
-    },
-    mounted(){
-        
+    data() {
+        return {
+            currentPage: 1
+        };
     },
     methods: {
-        active(item){
-            const act = document.querySelectorAll('.active')
-            act.forEach((item)=>{
-                if(item.classList.contains('active')){
-                    item.classList.remove('active')
-                }
-            })
-
-            for(let i = 0;i<=Math.ceil(this.pages/5);i++){
-                this.arr[i] = false;
-            }
-
-            this.arr[item] = !this.arr[item]
-            
-
-        }
+        active(item) {
+            this.currentPage = item;
+        },
     },
-}
+};
 </script>
 <style scoped>
-
+.page-link {
+    cursor: pointer;
+}
 </style>
